7cac6314773f45b994d8f70fb28c0bda9ca72f93
Robert Schmidt <rob@c7a.ca>
2010-05-18 16:08:02 +0000

    new ingest script and libs

---
+++ CAP/lib/Ingest.pm
@@ -0,0 +1,72 @@
+package Ingest;
+
+use strict;
+use warnings;
+use String::CRC32;
+use Data::Dumper;
+use File::Basename;
+use File::Copy;
+use File::Path qw(make_path);
+
+
+
+sub new 
+{
+    my $class = shift;
+    my (%config) =  @_;
+    my $self             = { };
+    $self->{config}= { %config };
+    #print Dumper($self);
+    bless ($self, $class);
+    return $self;
+}
+
+
+sub get_path
+{
+    # Given a filename return the full filesystem path
+    
+    my($self, $file, $contributor) = @_;
+    my $file_name = basename($file);
+    my $digest = String::CRC32::crc32($file_name)  ;
+    #print $digest."\n";
+    my $hex_prefix = sprintf("%X", $digest);
+    my $prefix = substr($hex_prefix, 0, 2) . '/' . substr($hex_prefix, 2, 2);
+    #print $prefix."\n";
+
+    my $repos = $self->{config}->{content};
+
+    my $path = "$repos/$contributor/$prefix";
+
+    return $path;
+    
+}
+
+sub get_fqfn
+{
+    my($self, $file, $contributor) = @_;
+
+    my $path = $self->get_path($file,$contributor);
+    
+    my $file_name = basename($file);
+    my $fqfn = "$path/$file_name";
+    
+    return $fqfn;
+}
+
+sub ingest_file
+{
+    my($self, $file, $contributor)=@_;
+
+    my $path=$self->get_path($file, $contributor);
+    my $fqfn=$self->get_fqfn($file, $contributor);
+
+    unless (-d $path) {
+        make_path($path) or die("Failed to make $path: $!");
+    }
+    copy($file, $fqfn) or die("Failed to copy $file to $fqfn: $!");
+    
+    return $fqfn;
+
+}
+1;
