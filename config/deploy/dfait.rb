e8ef8137b500d1868104bce3f80d173870ec25d5
Robert Schmidt <rob@c7a.ca>
2010-05-18 20:32:38 +0000

    merge mainline into dfait

---
+++ config/deploy/dfait.rb
@@ -0,0 +1,15 @@
+# DEV-specific deployment configuration
+# please put general deployment config in config/deploy.rb
+
+role :appservers, "192.168.1.200"
+
+set :select_uri, "http://localhost:8985/solr/select"
+set :update_uri, "http://localhost:8985/solr/update"
+
+set :deploy_to, "/opt/cap-dfait/cap"
+set :app_var, "/opt/cap-dfait/cap-var"
+
+set :repository, "http://dev.cihm/svn/cap/branches/dfait"
+
+set :deploy_via, :remote_cache
+
