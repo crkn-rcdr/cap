1633fa632aa3a7d51469680ae6ae2413ab1e3cea
William Wueppelmann <william@c7a.ca>
2010-06-07 14:05:42 +0000

    Updated solr schema; translation fixes; disabled admin portal

--- tools/cmr-tools/build/oocihm/build.sh
+++ tools/cmr-tools/build/oocihm/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+base="/cihm/collections/cmr/oocihm/hfc"
+
 while read  file; do
     cihm=$(basename $file .xml)
     cihm=${cihm:5}
     echo $cihm
-    output="/cihm/collections/cmr/oocihm/ecl/$cihm.xml"
+    output="$base/$cihm.xml"
+    echo $output
     if [ -f $output ]; then
         echo "$output exists; skipping"
     else
-        ./mkcmr cmr.xsd config/eco2.conf $file /cihm/collections/cmr/oocihm/ecl/$cihm.xml
+        ./mkcmr cmr.xsd config/eco2.conf $file $output
     fi
 done < "$1"
 
