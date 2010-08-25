ec1c9c3e666b20c5660ea92f8c361837c68041cb
Robert Schmidt <rob@c7a.ca>
2010-08-25 15:30:11 +0000

    new deploy for alpha

---
+++ config/deploy/alpha.rb
@@ -0,0 +1,16 @@
+# DEV-specific deployment configuration
+# please put general deployment config in config/deploy.rb
+
+role :appservers, "asiago.cihm", "brie.cihm"
+
+set :select_uri, "http://localhost:8989/solr/select"
+set :update_uri, "http://localhost:8989/solr/update"
+
+set :deploy_to, "/opt/cap-alpha/cap"
+set :app_var, "/opt/cap-alpha/cap-var"
+set :debug_var, "true"
+
+set :repository, "http://dev.cihm/svn/cap/branches/alpha"
+
+set :deploy_via, :remote_cache
+
