48ead1223b6fe9cc718cb314006fb8fc81f94090
William Wueppelmann <william@c7a.ca>
2011-02-07 14:13:12 +0000

    cap.conf handling updates; fr.po file update

--- CAP/lib/CAP/I18N/gettext.sh
+++ CAP/lib/CAP/I18N/gettext.sh
@@ -2,12 +2,19 @@
 # Run this script from the lib/CAP/I18N directory to update the fr.po
 # catalog.
 
+caproot=$1;
+
 pofile=fr.po
 
-cp $pofile $pofile.bak
-if [ $? != 0 ]; then
-    echo Failed to back up $pofile: $?
-    exit $?
-fi
 
-find ../../../root -name '*.tt' | xargs xgettext.pl -o $pofile
+if [ -d $caproot ]; then
+    cp $pofile $pofile.bak
+    if [ $? != 0 ]; then
+        echo Failed to back up $pofile: $?
+        exit $?
+    fi
+
+    find $caproot -name '*.tt' | xargs xgettext.pl -o $pofile
+else
+    echo "Usage $0 PAT_TO_CAP_ROOT"
+fi
