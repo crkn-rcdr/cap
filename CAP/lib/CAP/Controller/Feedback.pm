3ac0ecb0489d5dde510b80a6a7bfb786e5bb323f
Milan Budimirovic <milan.budimirovic@canadiana.ca>
2011-12-22 19:00:25 +0000

    added feedback module

---
+++ CAP/lib/CAP/Controller/Feedback.pm
@@ -0,0 +1,45 @@
+package CAP::Controller::Feedback;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+CAP::Controller::Feedback - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    # $c->response->body('Matched CAP::Controller::Feedback in Feedback.');
+    $c->stash->{template} = "feedback.tt";
+}
+
+
+=head1 AUTHOR
+
+Milan Budimirovic,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
