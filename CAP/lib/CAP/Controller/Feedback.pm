eb95241c446d5639e50e1672345c08b30f487349
Milan Budimirovic <milan.budimirovic@canadiana.ca>
2012-01-13 20:49:50 +0000

    updated feedback module

--- CAP/lib/CAP/Controller/Feedback.pm
+++ CAP/lib/CAP/Controller/Feedback.pm
@@ -27,6 +27,15 @@ sub index :Path :Args(0) {
     # $c->response->body('Matched CAP::Controller::Feedback in Feedback.');
     $c->stash->{template} = "feedback.tt";
     my $user_comments = defined($c->request->params->{feedback}) ? $c->request->params->{feedback} : "";
+    $c->stash->{feedback_submitted} = 0;
+    
+    if (length($user_comments)) {
+        $c->stash->{feedback_submitted} = 1;
+    }
+    
+    
+    return 1;
+    
 }
 
 
