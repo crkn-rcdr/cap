d9cd3a36a90d3e8c730e0f0388530730b6b0d58c
Russell McOrmond <russell.mcormond@canadiana.ca>
2012-01-23 21:04:56 +0000

    Added in ability to attach files to Mail->sendmail, made the From: configurable, added in new subscription_taxreceipt()

--- CAP/lib/CAP/Controller/Mail.pm
+++ CAP/lib/CAP/Controller/Mail.pm
@@ -2,6 +2,9 @@ package CAP::Controller::Mail;
 use Moose;
 use namespace::autoclean;
 
+use File::MimeInfo;
+use File::Basename;
+
 BEGIN {extends 'Catalyst::Controller'; }
 
 =head1 NAME
@@ -22,28 +25,74 @@ Catalyst Controller.
 =cut
 
 sub sendmail {
-    my ($self, $c, $template, $header) = @_;
+    my ($self, $c, $template, $header, $attach) = @_;
 
     $c->stash(additional_template_paths => [
         join('/', $c->config->{root}, 'templates', 'Mail', $c->stash->{portal}),
         join('/', $c->config->{root}, 'templates', 'Mail', 'Common')
     ]);
 
-    $c->email(
-        {
-            header => $header,
-            body => $c->view("Mail")->render($c, $template)
-        }
-    );
 
+    if (defined($attach)) {
+	# Assumes a single file name. Will need update if we want to 
+        # accept array of filenames
+
+	my $attachtype = mimetype($attach);
+	my $attachbody = "";
+	local $/ = undef;
+	if (open FILE, $attach) {
+	    binmode FILE;
+	    $attachbody = <FILE>;
+	    close FILE;
+	} else {
+# TODO: Decide what to do if we can't open file.
+	    $c->log->error("Can't open attachment $attach : $!\n");	    
+	}
+
+	my $textbody = $c->view("Mail")->render($c, $template);
+
+	# Two part MIM message:  plain text + attachment
+	my @parts = (
+	    Email::MIME->create (
+		attributes => {
+		    content_type => 'text/plain',
+		    charset      => 'UTF-8',
+		},
+		body => $textbody,
+	    ),
+	    Email::MIME->create(
+		attributes => {
+		    content_type => $attachtype,
+		    encoding     => 'quoted-printable',
+		    name         => basename($attach),
+		},
+		body => $attachbody,
+	    ),
+	);
+
+	$c->email({
+		header => $header,
+		parts => \@parts,
+	    });
+
+    } else {
+	# Send simple email
+	$c->email({
+		header => $header,
+		body => $c->view("Mail")->render($c, $template)
+	    });
+    }
     return 1;
 }
 
 sub user_reset :Private {
     my ($self, $c, $recipient, $confirm_link) = @_;
     $c->stash(confirm_link => $confirm_link);
+
+    my $from = $c->config->{email_from} || 'info@canadiana.ca'; 
+
     my $header = [
-        From => 'info@canadiana.ca',
+        From => $from,
         To => $recipient,
         Subject => $c->loc('Password Reset')
     ];
@@ -59,8 +108,9 @@ sub user_activate :Private {
         confirm_link => $confirm_link
     );
 
+    my $from = $c->config->{email_from} || 'info@canadiana.ca'; 
     my $header = [
-        From => 'info@canadiana.ca',
+        From => $from,
         To => $recipient,
         Subject => $c->loc('ECO Account Activation')
     ];
@@ -77,13 +127,28 @@ sub subscription_notice :Private {
         subscribe_message => $message
     );
 
+    my $from = $c->config->{email_from} || 'info@canadiana.ca'; 
     my $header = [
-        From => 'info@canadiana.ca',
+        From => $from,
         To => $admins,
         Subject => $c->loc('ECO Subscription Finalized')
     ];
 
     $self->sendmail($c, "subscribe_finalize.tt", $header);
+    return 1;
+}
+
+sub subscription_taxreceipt :Private {
+    my ($self, $c, $email, $name, $receiptfile) = @_;
+
+    my $from = $c->config->{email_from} || 'info@canadiana.ca'; 
+    my $header = [
+        From => $from,
+        To => $email,
+        Subject => $c->loc('ECO Tax Receipt')
+    ];
+
+    $self->sendmail($c, "subscribe_taxreceipt.tt", $header, $receiptfile);
     return 1;
 }
 
