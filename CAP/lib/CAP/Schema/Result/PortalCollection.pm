245f28c381897defe97f897624c7625268142721
no author <sysadmin@c7a.ca>
2012-08-08 18:57:19 +0000

    Primary key for portal collection

--- CAP/lib/CAP/Schema/Result/PortalCollection.pm
+++ CAP/lib/CAP/Schema/Result/PortalCollection.pm
@@ -42,14 +42,16 @@ __PACKAGE__->table("portal_collection");
 =head2 portal_id
 
   data_type: 'varchar'
+  default_value: (empty string)
   is_foreign_key: 1
-  is_nullable: 1
+  is_nullable: 0
   size: 64
 
 =head2 collection_id
 
   data_type: 'varchar'
-  is_nullable: 1
+  default_value: (empty string)
+  is_nullable: 0
   size: 32
 
 =head2 hosted
@@ -62,12 +64,32 @@ __PACKAGE__->table("portal_collection");
 
 __PACKAGE__->add_columns(
   "portal_id",
-  { data_type => "varchar", is_foreign_key => 1, is_nullable => 1, size => 64 },
+  {
+    data_type => "varchar",
+    default_value => "",
+    is_foreign_key => 1,
+    is_nullable => 0,
+    size => 64,
+  },
   "collection_id",
-  { data_type => "varchar", is_nullable => 1, size => 32 },
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 32 },
   "hosted",
   { data_type => "tinyint", default_value => 0, is_nullable => 0 },
 );
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</portal_id>
+
+=item * L</collection_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("portal_id", "collection_id");
 
 =head1 RELATIONS
 
@@ -86,8 +108,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07025 @ 2012-07-19 15:49:00
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+9kU4UBhgTebLxo80thgPQ
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2012-08-08 14:53:08
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:y17kfa0KnHJYQ7vED6GpDw
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
