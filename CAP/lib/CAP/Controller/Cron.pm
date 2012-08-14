1aebf0d87537ce5f63306250e8a0da2ef7ec8277
no author <sysadmin@c7a.ca>
2012-08-14 17:55:21 +0000

    Don't getnerate tax receipt or expiry emails unless $production is true.

--- CAP/lib/CAP/Controller/Cron.pm
+++ CAP/lib/CAP/Controller/Cron.pm
@@ -7,24 +7,34 @@ BEGIN {extends 'Catalyst::Controller'; }
 sub index :Path :Args(0) {
     my ($self, $c) = @_;
 
+    # Grab settings from cap_local.conf
+    my $production = $c->config->{production};
+
+
     # Cron must be called from localhost.
 # Temporarily disable
 #    if ($c->req->address ne '127.0.0.1') {
 #        $c->detach('/error', [403, "Request from unauthorized address"]);
 #    }
 
-    # Call various cron events
+    if ($production) {
+
+      # Generate tax receipt PDF's and email them
+      $c->forward('/cron/taxreceipt/index');
+
+      # Send email to users whose accounts are expiring
+      $c->forward('/cron/expiringtrialreminder/index');
+    	
+    } else {
+	$c->log->warn("Not production, some cron jobs skipped");
+    }
+
+    # Clear out old sessions
     $c->forward('/cron/session/index');
 
-    # Generate tax receipt PDF's and email them
-    $c->forward('/cron/taxreceipt/index');
-    
     # Delete unconfirmed accopunts
     $c->forward('/cron/removeunconfirmed/index');
     
-    # Delete unconfirmed accopunts
-    $c->forward('/cron/expiringtrialreminder/index');
-    	
     return 1;
 }
 
