2cc174d2a4d7431cee982d7bb1a6b52fdde915d1
no author <sysadmin@c7a.ca>
2012-08-24 19:57:28 +0000

    added subroutine ot check if user is an institution manager

---
+++ CAP/lib/CAP/Schema/ResultSet/InstitutionMgmt.pm
@@ -0,0 +1,34 @@
+package CAP::Schema::ResultSet::InstitutionMgmt;
+
+use strict;
+use warnings;
+use base 'DBIx::Class::ResultSet';
+
+
+sub is_inst_manager
+{
+    # returns the first active promo code
+    my($self, $userid, $inst) = @_;
+           
+    my $mgr_check =  $self->search(
+                                {
+
+                                
+                                    id    =>  $userid,
+                                    inst  =>  $inst
+                                  
+                                }           
+                             );
+
+    my $is_mgr = defined($mgr_check->next) ? 1 : 0;
+
+    return $is_mgr;
+}
+
+
+
+
+
+
+1;
+
