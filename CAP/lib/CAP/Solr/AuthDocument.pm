2a60c94c80f482cd2e162fd616d89b54ccfcec4c
William Wueppelmann <william@c7a.ca>
2012-11-28 19:38:52 +0000

    AuthDocument uses the user_subscription table to determine permissions

--- CAP/lib/CAP/Solr/AuthDocument.pm
+++ CAP/lib/CAP/Solr/AuthDocument.pm
@@ -44,69 +44,80 @@ method BUILD {
 
 method authorize ($portal, $user, $institution) {
 
-    if ($portal->id eq 'eco') {
-        my $subscriber = 0;
-        my $paid       = 0;
-        if ($user) {
-            $subscriber = 1 if ($user->subexpires && $user->subexpires->epoch() >= time);
-            $paid = 1 if ($user->subexpires && $user->subexpires->epoch() >= time && $user->class eq 'paid');
-            $subscriber = 1 if ($user->class eq 'permanent');
-            $paid = 1 if ($user->class eq 'permanent');
+    # TODO: this is still a temporary workaround to having a set of
+    # database-driven rules. It works as long as 'eco' is the only portal
+    # that does not provide full open access to all content. This should
+    # be replaced by a set of generic rule checks with all of the specific
+    # parameters being defined in a database table.
+
+    # Initialize the user's subscription level to 0 (none)
+    my $level = 0;
+
+    # Set the user's subscription level based on
+    if ($user) {
+        my $subscription = $user->find_related('user_subscriptions', { portal_id => $portal });
+        # If the user has no subscription for this portal, access level
+        # remains 0.
+        if (! $subscription) {
+            $level = 0;
         }
-        if ($institution) {
-            if ($institution->is_subscriber($portal)) {
-                $subscriber = 1;
-                $paid = 1;
-            }
+        # If the user's subscription is permanent, get the subscription level
+        elsif ($subscription->permanent) {
+            $level = $subscription->level;
         }
-        if ($subscriber) {
-            $self->auth->all_pages(1);
-            $self->auth->view_all(1);
-            $self->auth->view_part(1);
-            $self->auth->download(1) if ($paid);
-            $self->auth->resize(1);
-            if ($self->record_type eq 'document') {
-                for (my $page = 0; $page < $self->child_count; ++$page) {
-                    $self->auth->addPage(1);
-                }
-            }
+        # If the subscription has not yet expired, the the subscription level
+        elsif ($subscription->expires->epoch() >= time) {
+            $level = $subscription->level;
         }
-        else {
-            $self->auth->all_pages(0);
-            $self->auth->view_all(0);
-            $self->auth->view_part(1);
-            $self->auth->download(0);
-            $self->auth->resize(0);
-            if ($self->record_type eq 'document') {
-                for (my $page = 0; $page < $self->child_count; ++$page) {
-                    # The first page is always allowed (in case there is only one # page)
-                    if ($page == 1) {
-                        $self->auth->addPage(1);
-                    }
-                    # If this is a series, the first 2 issues are open to all.
-                    elsif ($self->seq && $self->seq <= 2) {
-                        $self->auth->addPage(1);
-                    }
-                    # The first 20 pages or 50% (whichever is less) are accessible to all.
-                    elsif ($page < 20 && $page <= int($self->child_count / 2)) {
-                        $self->auth->addPage(1);
-                    }
-                    else {
-                        $self->auth->addPage(0);
-                    }
-                }
+    }
+
+    # If we have an institutional subscription, the subscription level is
+    # set to 2 (maximum)
+    if ($institution) {
+        if ($institution->is_subscriber($portal)) {
+            $level = 2;
+        }
+    }
+
+    # All portals other than ECO give the user an effective access level of 2.
+    if ($portal->id ne 'eco') {
+        $level = 2;
+    }
+
+    warn "Access Level: $level\n";
+
+    # Set to true if the user's access level is equal to or greater
+    # than the required level.
+    $self->auth->all_pages(int($level >= 1));
+    $self->auth->view_all(int($level >= 1));
+    $self->auth->view_part(int($level >= 1));
+    $self->auth->download(int($level >= 2));
+    $self->auth->resize(int($level >= 1));
+    
+    # Level 1 gives access to all pages, level 0 to only a preview.
+    if ($level >= 1) {
+        if ($self->record_type eq 'document') {
+            for (my $page = 0; $page < $self->child_count; ++$page) {
+                $self->auth->addPage(1);
             }
         }
     }
     else {
-        $self->auth->all_pages(1);
-        $self->auth->view_all(1);
-        $self->auth->view_part(1);
-        $self->auth->download(1);
-        $self->auth->resize(1);
-        if ($self->record_type eq 'document') {
-            for (my $page = 0; $page < $self->child_count; ++$page) {
+        for (my $page = 0; $page < $self->child_count; ++$page) {
+            # The first page is always allowed (in case there is only one # page)
+            if ($page == 1) {
                 $self->auth->addPage(1);
+            }
+            # If this is a series, the first 2 issues are open to all.
+            elsif ($self->seq && $self->seq <= 2) {
+                $self->auth->addPage(1);
+            }
+            # The first 20 pages or 50% (whichever is less) are accessible to all.
+            elsif ($page < 20 && $page <= int($self->child_count / 2)) {
+                $self->auth->addPage(1);
+            }
+            else {
+                $self->auth->addPage(0);
             }
         }
     }
