f65a99d2ff1ee532392e3a5090f89d0ccfa78bcf
William Wueppelmann <william@c7a.ca>
2013-03-14 14:38:47 +0000

    Content management

--- CAP/lib/CAP/Controller/Content.pm
+++ CAP/lib/CAP/Controller/Content.pm
@@ -50,31 +50,43 @@ sub index :Path :Args(0) {
     my ($self, $c) = @_;
 
     my @portal_content = $c->model('DB::PortalsTitles')->counts_by_portal();
-    my $unassigned = $c->model('DB::Titles')->unassigned();
 
     $c->stash(
-        portal_content => \@portal_content,
-        unassigned_count => $unassigned->count
+        portal_titles => \@portal_content,
+        contributor_titles => $c->model('DB::Titles')->counts_by_institution,
     );
 
     return 1;
 }
 
-=head2 unassigned
+=head2 unassigned($contributor)
 
-List titles that have not been assigned to any portals
+List titles belonging to the specified institution that have not been assigned to any portals.
 
 =cut
-sub unassigned :Path('unassigned') :Args(0) {
-    my ($self, $c) = @_;
+sub unassigned :Path('unassigned') :Args(1) {
+    my ($self, $c, $institution_id) = @_;
+
+    my $institution = $c->model('DB::Institution')->find({ id => $institution_id});
+    if (! $institution) {
+        $c->message({ type => "error", message => "institution_not_found" });
+        $self->status_not_found( $c, message => "No such institution");
+        return 1;
+    }
 
     $c->stash(
-        unassigned => [$c->model('DB::Titles')->unassigned()->all]
+        unassigned => [$c->model('DB::Titles')->unassigned($institution_id)->all],
+        institution => $institution
     );
 
     return 1;
 }
 
+=head2 manage($id)
+
+Display or update the portal assignments for title $id.
+
+=cut
 sub manage :Local Path('manage') Args(1) ActionClass('REST') {
     my($self, $c, $id) = @_;
 }
@@ -97,6 +109,53 @@ sub manage_GET {
     return 1;
 }
 
+sub manage_POST {
+    my($self, $c, $id) = @_;
+
+    my $title = $c->model('DB::Titles')->find({ id => $id });
+    if (! $title) {
+        $c->message({ type => "error", message => "title_not_found" });
+        $self->status_not_found( $c, message => "No such title");
+        return 1;
+    }
+
+    my @portals = $c->model('DB::Portal')->search({})->all;
+
+    my %data;
+    foreach my $key (keys %{$c->req->body_parameters}) {
+        $data{$key} = $c->req->body_parameters->{$key} if $c->req->body_parameters->{$key};
+    }
+
+    foreach my $portal (@portals) {
+        my $value = int($c->req->body_parameters->{"portal-" . $portal->id} || 0);
+
+        # Title should not be hosted by this portal
+        if ($value == 0) {
+            my $title = $c->model('DB::PortalsTitles')->find({ title_id => $id, portal_id => $portal->id});
+            $title->delete if ($title);
+        }
+
+        # Title is indexed for searching but not hosted
+        elsif ($value == 1) {
+            $c->model('DB::PortalsTitles')->update_or_create(
+                title_id => $id,
+                portal_id => $portal->id,
+                hosted => 0
+            );
+        }
+
+        # Title is indexed and hosted
+        elsif ($value == 2) {
+            $c->model('DB::PortalsTitles')->update_or_create(
+                title_id => $id,
+                portal_id => $portal->id,
+                hosted => 1
+            );
+        }
+    }
+
+    $c->response->redirect($c->uri_for_action('/content/manage', $id));
+}
 
 =head1 AUTHOR
 
--- CAP/lib/CAP/Schema/ResultSet/Titles.pm
+++ CAP/lib/CAP/Schema/ResultSet/Titles.pm
@@ -12,17 +12,52 @@ use base 'DBIx::Class::ResultSet';
 
 =head2 unassigned
 
-Returns the set of all titles that are not currently assigned to any portal
+Returns the set of all titles for the supplied institution that are not assigned to any portal.
 
 =cut
 sub unassigned {
-    my($self) = @_;
+    my($self, $institution_id) = @_;
     my $result = $self->search(
-        { portal_id => { '=' => undef } },
+        { institution_id => $institution_id, portal_id => { '=' => undef } },
         { join => 'portals_titles' }
     );
     return $result;
 }
 
+=head2 count_by_institution
+
+Return a list of institutions that own one or more titles along with the
+total number of titles and the number of titles not assigned to any
+portals.
+
+[ { institution => $inst, titles => $titles, unassigned => $unassigned }, ... ]
+
+=cut
+sub counts_by_institution {
+    my($self) = @_;
+    my @counts = ();
+
+    # A set of institutions with the corresponding number of titles
+    # belonging to them, by title count in descending order.
+    my $institutions = $self->search(
+        {},
+        {
+            select => [ 'institution_id', { count => 'id', -as => 'title_count' } ],
+            as     => [ 'institution_id', 'title_count' ],
+            group_by => [ 'institution_id' ],
+            order_by => [ { -desc => 'title_count' } ]
+        }
+    );
+
+    while (my $i = $institutions->next) {
+        push(@counts, {
+            institution => $i->institution_id,
+            titles => $i->get_column('title_count'),
+            unassigned => $self->unassigned($i->institution_id->id)->count
+        });
+    }
+
+    return \@counts;
+}
 
 1;
