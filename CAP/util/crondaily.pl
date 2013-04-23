f98deb4842bbeb13610f3231685a0f3675c535ed
Milan Budimirovic <milan.budimirovic@canadiana.ca>
2013-04-23 15:23:55 +0000

    updated crondaily.pl to check if the machine is in production before sending mail

--- CAP/util/crondaily.pl
+++ CAP/util/crondaily.pl
@@ -66,6 +66,10 @@ sub expiring_subscription_reminder {
     
     
     my $c = shift();
+    
+    # Generally we don't want to run this subroutine unless we're on a production server
+    # or on the workstation of the maintainer
+    return 1 unless ( $c->config->{productionflagfile} && -e $c->config->{productionflagfile} );
    
     $c->model('DB::CronLog')->create({
                action  => 'crondaily',
