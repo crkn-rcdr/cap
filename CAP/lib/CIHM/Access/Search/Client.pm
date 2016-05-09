6188be1f7d2560fdbbf9074a90764bd4ddd49583
Sascha Adler <sascha.adler@canadiana.ca>
2016-05-09 21:52:59 +0000

    Fields and filters work. Might refactor this junk to a separate Query class

--- CAP/lib/CIHM/Access/Search/Client.pm
+++ CAP/lib/CIHM/Access/Search/Client.pm
@@ -27,28 +27,7 @@ has 'root_collection' => (
 	required => 1
 );
 
-# Parameters:
 
-# q#: the search query (divided into or_group q0, q1, q2, etc.)
-
-# or_group = term, { or_divider, term } ;
-# term = ["-"], [field_modifier, ":"], word | phrase ;
-# field_modifier = "ti" | "au" | "pu" | "su" | "tx" | "no" ;
-# word = character, { character } ;
-# phrase = '"', { every_character - ('"' | or_divider) }, '"' ;
-# or_divider = white_space, "|", white_space
-
-# character = every_character - white_space_character ;
-# white_space = white_space_character, { white_space_character }
-# white_space_character = ? white space characters ? ;
-# every_character = ? all visible characters ? ;
-
-# filter parameters (q):
-# df/dt: date filtering
-# lang
-# pkey
-# identifier
-# depositor
 
 # sorting parameter:
 # sort
@@ -62,34 +41,89 @@ sub translate_query_to_solr {
 	return $solr_params;
 }
 
-# query terms keys look like q0, q1, q2, etc.
+# Parameters:
+
+# q#|#: query terms (see below)
+
+# filter parameters 
+# collection
+# df/dt: date filtering
+# lang
+# pkey
+# identifier
+# depositor
 sub _build_query_terms {
 	my ($self, $query_params) = @_;
-	my @or_groups = ();
+	my @terms = ();
 	foreach my $or_group ($self->_sorted_query_values($query_params)) {
-		my @terms = ();
-		foreach my $term (split /\s+\|\s+/, $or_group) {
-			my $analyzed_term = $self->_analyze_term($term);
-			push @terms, $analyzed_term if ($analyzed_term);
-		}
-		if (@terms) {
-			my $joined_or_group = join ' OR ', @terms;
-			push @or_groups, @terms > 1 ? "($joined_or_group)" : $joined_or_group;
-		}
+		my $joined_group = _skip_join(' OR ', @{$or_group});
+		$joined_group = "($joined_group)" if (@{$or_group} > 1);
+		push @terms, $joined_group;
 	}
 
-	return join ' ', @or_groups;
+	foreach my $filter (keys %{$self->schema->filters}) {
+		push @terms, $self->_filter_query($filter, $query_params->{$filter});
+	}
+
+	return _skip_join(' ', @terms);
 }
 
+sub _filter_query {
+	my ($self, $filter, $param) = @_;
+
+	return '' unless ($param);
+
+	if (ref($param) eq 'ARRAY') {
+		return map { $self->_filter_query($filter, $_) } @{$param};
+	}
+
+	my $filter_opts = $self->schema->filters->{$filter};
+	my $template = $filter_opts->{template} || "$filter:\$";
+	$template =~ s/\$/$param/g;
+	return $template;
+}
+
+# join, but ignore empty strings
+sub _skip_join {
+	my ($separator, @list) = @_;
+	return reduce { length $a && length $b ? "$a$separator$b" :
+                    length $a              ? $a                 :
+                    length $b              ? $b                 :
+                    '' } '', @list;
+}
+
+# query term keys are of two possible forms:
+# q$x if not part of an or group
+# q$x|$y if part of an or group
 sub _sorted_query_values {
 	my ($self, $query_params) = @_;
-	return 
-		map { $query_params->{$_} }
-		sort { ($a =~ /q(\d+)/)[0] <=> ($b =~ /q(\d+)/)[0] }
-		grep { /q(\d+)/ }
-		keys $query_params;
+	my $key_exp = qr/^q(\d+)(?:\|(\d+))?$/;
+	my @value_index = map {
+		$_ =~ /$key_exp/;
+		defined $1 ? [$1, $2, $query_params->{$_}] : ()
+	} keys $query_params;
+
+	my @sort = ();
+	foreach (@value_index) {
+		my ($x, $y, $value) = @{$_};
+		$y ||= 0;
+		$sort[$x] = [] unless (defined $sort[$x]);
+		$sort[$x][$y] = $self->_analyze_term($value);
+	}
+
+	return @sort;
 }
 
+# term = ["-"], [field_modifier, ":"], word | phrase ;
+# field_modifier = "ti" | "au" | "pu" | "su" | "tx" | "no" ;
+# word = character, { character } ;
+# phrase = '"', { every_character - ('"' | or_divider) }, '"' ;
+# or_divider = white_space, "|", white_space
+
+# character = every_character - white_space_character ;
+# white_space = white_space_character, { white_space_character }
+# white_space_character = ? white space characters ? ;
+# every_character = ? all visible characters ? ;
 sub _analyze_term {
 	my ($self, $term) = @_;
 	my ($negation, $field_modifier, $token) = $term =~ /
@@ -107,16 +141,16 @@ sub _analyze_term {
 	return '' if ($field_modifier && !exists $self->schema->fields->{$field_modifier});
 
 	$token = $self->_parse_token($token);
-	my $result = '';
-	$result .= '-' if ($negation);
-	$result .= '(';
-	$result .=
+	my @result;
+	push(@result, '-') if ($negation);
+	push @result, '(';
+	push @result,
 		reduce { $a . ' OR ' . $b }
 		map { "$_:$token" }
 		@{$self->schema->fields->{$field_modifier}};
-	$result .= ')';
+	push @result, ')';
 
-	return $result;
+	return join '', @result;
 
 }
 
--- CAP/t/access_search.t
+++ CAP/t/access_search.t
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 use Test::More;
+use Test::Deep;
 
 BEGIN { use_ok 'CIHM::Access', 'CIHM::Access::Search::Client' }
 
@@ -20,6 +21,10 @@ is $client->_analyze_term('su:""'), '', 'ignore empty phrases after field modifi
 is $client->_analyze_term('borg:plook'), '', 'ignore bad field modifiers';
 is $client->_analyze_term('^*)@$('), '(gq:\^*\)@$\( OR tx:\^*\)@$\()', 'escape special characters';
 
+is $client->_filter_query('collection', 'hfc'), 'collection:hfc', 'handle query filter';
+is $client->_filter_query('df', '1850'), 'pubmax:[1850-01-01T00:00:00.000Z TO *]', 'handle query filter with custom template';
+is(($client->_filter_query('identifier', ['P04', 'S01']))[1], 'identifier:S01', 'handle multiple filters from same key');
+
 is $client->_build_query_terms({
 	q0 => 'hi'
 }), '(gq:hi OR tx:hi)', 'handle regular term';
@@ -27,8 +32,8 @@ is $client->_build_query_terms({
 	q0 => 'su:"upper canada"'
 }), '(su:"upper canada")', 'handle regular phrase';
 is $client->_build_query_terms({
-	q0 => 'hi | su:bye | -pu:why', q1 => 'hello'
-}), '((gq:hi OR tx:hi) OR (su:bye) OR -(pu:why)) (gq:hello OR tx:hello)', 'handle or phrase';
+	'q0|0' => 'hi', 'q0|1' => 'su:bye', 'q0|2' => '-pu:why', q1 => 'hello'
+}), '((gq:hi OR tx:hi) OR (su:bye) OR -(pu:why)) (gq:hello OR tx:hello)', 'handle or terms';
 is $client->_build_query_terms({
 	q0 => 'hi', q1 => 'su:bye', q2 => '-pu:why'
 }), '(gq:hi OR tx:hi) (su:bye) -(pu:why)', 'handle multiple regular terms';
@@ -41,5 +46,11 @@ is $client->_build_query_terms({
 is $client->_build_query_terms({
 	q0 => 'hi', q1 => '#$89', q2 => 'su:bye', q3 => 'au:'
 }), '(gq:hi OR tx:hi) (gq:#$89 OR tx:#$89) (su:bye)', 'discard garbage terms';
+
+# I can't know which order this comes in
+cmp_deeply $client->_build_query_terms({
+	q0 => 'hi', df => '1850', collection => 'hfc'
+}), any('(gq:hi OR tx:hi) pubmax:[1850-01-01T00:00:00.000Z TO *] collection:hfc',
+        '(gq:hi OR tx:hi) collection:hfc pubmax:[1850-01-01T00:00:00.000Z TO *]'), 'handle filters';
 
 done_testing;
