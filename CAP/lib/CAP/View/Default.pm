452370c1f22a0eb164d7415f279156797bc2488e
Sascha Adler <sascha.adler@canadiana.ca>
2016-09-02 17:15:18 +0000

    Implements a cdn_uri helper

--- CAP/lib/CAP/View/Default.pm
+++ CAP/lib/CAP/View/Default.pm
@@ -70,6 +70,11 @@ __PACKAGE__->config(
 
         current_year => sub {
             return time2str("%Y", time);
+        },
+
+        cdn_uri => sub {
+            my ($doc, $file) = (@_);
+            return "http://cdn.canadiana.ca/$doc/$file";
         }
     }
 );
