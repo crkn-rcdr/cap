aae1d8e9d4afbfc0e868f8d455d140fe9eae841b
Sascha Adler <sascha.adler@canadiana.ca>
2016-12-01 21:41:45 +0000

    Adds log_rename.pl, which renames logfiles to add their date to the filename (#5057)

---
+++ CAP/util/log_rename.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+
+my ($dir) = @ARGV;
+
+unless (defined $dir) {
+	print "Usage: log_rename.pl <directory>\n";
+	exit 1;
+}
+
+unless (-d $dir) {
+	print "Directory $dir does not exist\n";
+	exit 1;
+}
+
+my $pattern = qr/.*\.\d+$/;
+
+opendir my($dh), $dir;
+chdir $dh;
+foreach my $file (grep /$pattern/, readdir $dh) {
+	next if -d $file;
+	open my $fh, '<', $file;
+	my $line = <$fh>;
+	close $fh;
+
+	$line =~ /^(\d{4}-\d{2}-\d{2})/;
+	my $date = $1;
+	next unless $date;
+
+	$file =~ /(.*)$pattern/;
+	my $new_file = "$date-$1";
+	print "Renaming $file to $new_file\n";
+	rename $file, $new_file;
+}
+closedir $dh;
+
+exit;
