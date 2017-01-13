8590d0c39670b923f36e84c63cb11d5e648a7502
Sascha Adler <sascha.adler@canadiana.ca>
2017-01-13 19:29:08 +0000

    log_rename.pl reads the last line of the log file instead of the first to ascertain the date, since the first line could be a holdover from the previous date #5075

--- CAP/util/log_rename.pl
+++ CAP/util/log_rename.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use utf8;
 
+use File::ReadBackwards;
+
 my ($dir) = @ARGV;
 
 unless (defined $dir) {
@@ -22,9 +24,11 @@ opendir my($dh), $dir;
 chdir $dh;
 foreach my $file (grep /$pattern/, readdir $dh) {
 	next if -d $file;
-	open my $fh, '<', $file;
-	my $line = <$fh>;
-	close $fh;
+
+	# get the last line of the file, since we can't trust the first
+	my $fh = File::ReadBackwards->new($file);
+	my $line = $fh->readline;
+	$fh->close;
 
 	$line =~ /^(\d{4}-\d{2}-\d{2})/;
 	my $date = $1;
