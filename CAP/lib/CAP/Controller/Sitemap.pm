816fb156204adf427319eb19bdebb9696568d4b4
Sascha Adler <sadler@crkn.ca>
2020-01-15 11:08:11 -0700

    Gets rid of the CMS. Blocks and pages are stored as templates. Also gets rid of CouchCache, since only the CMS was using that.

--- CAP/lib/CAP/Controller/Sitemap.pm
+++ CAP/lib/CAP/Controller/Sitemap.pm
@@ -17,143 +17,168 @@ Catalyst Controller.
 
 =cut
 
-
 =head2 index
 
 Generate a sitemap page. Takes an argument "$n.xml" where $n is a number.
 
 =cut
-sub index :Path('') Args(1) {
-    my($self, $c, $file) = @_;
 
-    # Get the page number to generate from the file
-    my($page, $suffix) = split(/\./, $file);
-    unless ($page =~ /^\d+$/ && $page > 0 && $suffix eq 'xml') {
-        $c->detach('/error', [500]);
+sub index : Path('') Args(1) {
+  my ( $self, $c, $file ) = @_;
+
+  # Get the page number to generate from the file
+  my ( $page, $suffix ) = split( /\./, $file );
+  unless ( $page =~ /^\d+$/ && $page > 0 && $suffix eq 'xml' ) {
+    $c->detach( '/error', [500] );
+  }
+
+  my $titles;
+  eval {
+    $titles =
+      $c->model('Access::Presentation')->title_list( $c->portal_id, $page );
+  };
+  $c->detach( '/error', [500, $@] ) if $@;
+
+  # Create the sitemap file
+  my $doc  = XML::LibXML::Document->new( '1.0', 'UTF-8' );
+  my $root = $doc->createElement('urlset');
+  $root->setAttribute( 'xmlns',
+    'http://www.sitemaps.org/schemas/sitemap/0.9' );
+  $doc->setDocumentElement($root);
+
+  foreach my $title (@$titles) {
+    my $url = $doc->createElement('url');
+    my $loc = $doc->createElement('loc');
+
+    $loc->appendChild(
+      $doc->createTextNode(
+        $c->uri_for_action( 'view/index', $title->{key} )
+      )
+    );
+    $url->appendChild($loc);
+
+    # Add an update time if there is one
+    if ( $title->{updated} ) {
+      my $lastmod = $doc->createElement('lastmod');
+      $lastmod->appendChild( $doc->createTextNode( $title->{updated} ) );
+      $url->appendChild($lastmod);
     }
 
-    my $titles;
-    eval {
-        $titles = $c->model('Access::Presentation')->title_list($c->portal_id, $page);
-    };
-    $c->detach('/error', [500, $@]) if $@;
+    $root->appendChild($url);
+  }
 
-    # Create the sitemap file
-    my $doc = XML::LibXML::Document->new('1.0', 'UTF-8');
-    my $root = $doc->createElement('urlset');
-    $root->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-    $doc->setDocumentElement($root);
-
-    foreach my $title (@$titles) {
-        my $url = $doc->createElement('url');
-        my $loc = $doc->createElement('loc');
-
-        $loc->appendChild($doc->createTextNode($c->uri_for_action('view/index', $title->{key})));
-        $url->appendChild($loc);
-
-        # Add an update time if there is one
-        if ($title->{updated}) {
-            my $lastmod = $doc->createElement('lastmod');
-            $lastmod->appendChild($doc->createTextNode($title->{updated}));
-            $url->appendChild($lastmod);
-        }
-
-        $root->appendChild($url);
-    }
-
-    $c->res->header('Content-Type', 'application/xml');
-    $c->res->body($doc->toString(1));
-    return 1;
+  $c->res->header( 'Content-Type', 'application/xml' );
+  $c->res->body( $doc->toString(1) );
+  return 1;
 }
-
 
 =head 2 static
 
 Generate the file of static pages for the site
 
 =cut
-sub static :Path('static.xml') Args(0) {
-    my($self, $c) = @_;
 
-    # Create the sitemap file
-    my $doc = XML::LibXML::Document->new('1.0', 'UTF-8');
-    my $root = $doc->createElement('urlset');
-    $root->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-    $root->setAttribute('xmlns:xhtml', 'http://www.w3.org/1999/xhtml');
-    $doc->setDocumentElement($root);
+sub static : Path('static.xml') Args(0) {
+  my ( $self, $c ) = @_;
 
-    my $url = $doc->createElement('url');
-    my $loc = $doc->createElement('loc');
-    $loc->appendChild($doc->createTextNode($c->uri_for_action('index')));
+  # Create the sitemap file
+  my $doc  = XML::LibXML::Document->new( '1.0', 'UTF-8' );
+  my $root = $doc->createElement('urlset');
+  $root->setAttribute( 'xmlns',
+    'http://www.sitemaps.org/schemas/sitemap/0.9' );
+  $root->setAttribute( 'xmlns:xhtml', 'http://www.w3.org/1999/xhtml' );
+  $doc->setDocumentElement($root);
+
+  my $url = $doc->createElement('url');
+  my $loc = $doc->createElement('loc');
+  $loc->appendChild( $doc->createTextNode( $c->uri_for_action('index') ) );
+  $url->appendChild($loc);
+  $root->appendChild($url);
+
+  my $pages = $c->stash->{portal}->pages;
+  foreach my $path ( sort keys %$pages ) {
+    $url = $doc->createElement('url');
+    $loc = $doc->createElement('loc');
+    $loc->appendChild( $doc->createTextNode( $c->uri_for("/$path") ) );
     $url->appendChild($loc);
-    $root->appendChild($url);
 
-    my $nodes = $c->model('CMS')->sitemap($c->portal_id);
-    foreach my $path (sort keys %$nodes) {
-        $url = $doc->createElement('url');
-        $loc = $doc->createElement('loc');
-        $loc->appendChild($doc->createTextNode($c->uri_for("/$path")));
-        $url->appendChild($loc);
-        foreach my $alternate (@{ $nodes->{$path} }) {
-            my $link = $doc->createElement('xhtml:link');
-            $link->setAttribute('rel', 'alternate');
-            $link->setAttribute('hreflang', $alternate->[0]);
-            $link->setAttribute('href', $c->uri_for("/$alternate->[1]"));
-            $url->appendChild($link);
-        }
-        $root->appendChild($url);
+    my $mapping = $pages->{$path};
+    my @alternates;
+    if ( $mapping->{en} ) {
+      @alternates = ( ['en', $mapping->{en}], ['fr', $path] );
+    } elsif ( $mapping->{fr} ) {
+      @alternates = ( ['en', $path], ['fr', $mapping->{fr}] );
+    } else {
+      @alternates = ( ['en', $path], ['fr', $path] );
     }
 
-    $c->res->header('Content-Type', 'application/xml');
-    $c->res->body($doc->toString(1));
-    return 1;
-}
+    foreach my $alternate (@alternates) {
+      my $link = $doc->createElement('xhtml:link');
+      $link->setAttribute( 'rel',      'alternate' );
+      $link->setAttribute( 'hreflang', $alternate->[0] );
+      $link->setAttribute( 'href',     $c->uri_for("/$alternate->[1]") );
+      $url->appendChild($link);
+    }
 
+    $root->appendChild($url);
+  }
+
+  $c->res->header( 'Content-Type', 'application/xml' );
+  $c->res->body( $doc->toString(1) );
+  return 1;
+}
 
 =head2 sitemapindex
 
 Generate the sitemap index file
 
 =cut
-sub sitemapindex :Path('sitemap.xml') Args(0) {
-    my($self, $c) = @_;
-    my $map;
-    my $loc;
 
-    # Create the sitemap root file
-    my $doc = XML::LibXML::Document->new('1.0', 'UTF-8');
-    my $root = $doc->createElement('sitemapindex');
-    $root->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-    $doc->setDocumentElement($root);
+sub sitemapindex : Path('sitemap.xml') Args(0) {
+  my ( $self, $c ) = @_;
+  my $map;
+  my $loc;
 
-    # Add a link to the static sitemap
+  # Create the sitemap root file
+  my $doc  = XML::LibXML::Document->new( '1.0', 'UTF-8' );
+  my $root = $doc->createElement('sitemapindex');
+  $root->setAttribute( 'xmlns',
+    'http://www.sitemaps.org/schemas/sitemap/0.9' );
+  $doc->setDocumentElement($root);
+
+  # Add a link to the static sitemap
+  $map = $doc->createElement('sitemap');
+  $loc = $doc->createElement('loc');
+  $loc->appendChild(
+    $doc->createTextNode( $c->uri_for_action('sitemap/static') ) );
+  $map->appendChild($loc);
+  $root->appendChild($map);
+
+  # Determine how many sitemap files we need
+  my $map_length = $c->model('Access::Presentation')->sitemap_node_limit;
+  my $title_count;
+  eval {
+    $title_count =
+      $c->model('Access::Presentation')->title_count( $c->portal_id );
+  };
+  $c->detach( '/error', [500, $@] ) if $@;
+
+  for ( my $i = 0 ; $title_count > 0 ; $title_count -= $map_length ) {
     $map = $doc->createElement('sitemap');
     $loc = $doc->createElement('loc');
-    $loc->appendChild($doc->createTextNode($c->uri_for_action('sitemap/static')));
+    $loc->appendChild(
+      $doc->createTextNode(
+        $c->uri_for_action( 'sitemap/index', [sprintf( "%d.xml", ++$i )] )
+      )
+    );
     $map->appendChild($loc);
     $root->appendChild($map);
+  }
 
-    # Determine how many sitemap files we need
-    my $map_length = $c->model('Access::Presentation')->sitemap_node_limit;
-    my $title_count;
-    eval {
-        $title_count = $c->model('Access::Presentation')->title_count($c->portal_id);
-    };
-    $c->detach('/error', [500, $@]) if $@;
-
-    for (my $i = 0; $title_count > 0; $title_count -= $map_length) {
-        $map = $doc->createElement('sitemap');
-        $loc = $doc->createElement('loc');
-        $loc->appendChild($doc->createTextNode($c->uri_for_action('sitemap/index', [ sprintf("%d.xml", ++$i) ])));
-        $map->appendChild($loc);
-        $root->appendChild($map);
-    }
-
-    $c->res->header('Content-Type', 'application/xml');
-    $c->res->body($doc->toString(1));
-    return 1;
+  $c->res->header( 'Content-Type', 'application/xml' );
+  $c->res->body( $doc->toString(1) );
+  return 1;
 }
-
 
 =head1 AUTHOR
 
--- CAP/lib/CAP/Portal.pm
+++ CAP/lib/CAP/Portal.pm
@@ -9,41 +9,64 @@ use namespace::autoclean;
 extends 'CAP::Collection';
 
 has 'search' => (
-    is => 'ro',
-    isa => 'Bool',
-    required => 1,
-    default => 1
+  is       => 'ro',
+  isa      => 'Bool',
+  required => 1,
+  default  => 1
 );
 
 has 'search_schema' => (
-    is => 'ro',
-    isa => 'Str',
-    default => 'default'
+  is      => 'ro',
+  isa     => 'Str',
+  default => 'default'
 );
 
 has 'subcollections' => (
-    is => 'ro',
-    isa => 'HashRef',
-    default => sub { {} },
-    writer => '_set_subcollections'
+  is      => 'ro',
+  isa     => 'HashRef',
+  default => sub { {} },
+  writer  => '_set_subcollections'
+);
+
+has 'pages' => (
+  is      => 'ro',
+  isa     => 'HashRef',
+  default => sub { {} }
 );
 
 sub has_subcollections {
-    my ($self) = @_;
-    return scalar keys %{ $self->subcollections };
+  my ($self) = @_;
+  return scalar keys %{ $self->subcollections };
 }
 
 sub subcollection_labels {
-    my ($self, $lang) = @_;
-    return { map {
-        $_ => $self->subcollections->{$_}->label->{$lang}
-    } keys %{ $self->subcollections } };
+  my ( $self, $lang ) = @_;
+  return {
+    map { $_ => $self->subcollections->{$_}->label->{$lang} }
+      keys %{ $self->subcollections }
+  };
 }
 
 sub sorted_subcollection_keys {
-    my ($self, $lang) = @_;
-    my $labels = $self->subcollection_labels($lang);
-    return [ sort { $labels->{$a} cmp $labels->{$b} } keys %$labels ];
+  my ( $self, $lang ) = @_;
+  my $labels = $self->subcollection_labels($lang);
+  return [sort { $labels->{$a} cmp $labels->{$b} } keys %$labels];
+}
+
+sub page_mapping {
+  my ( $self, $lang, $key ) = @_;
+  my $mapping = $self->pages->{$key};
+  return undef if !$mapping;
+
+  if ( $mapping->{$lang} ) {
+    return { redirect => $mapping->{$lang} };
+  } else {
+    if ( $mapping->{en} ) {
+      return { page => $mapping->{en}, title => $mapping->{title}->{$lang} };
+    } else {
+      return { page => $key, title => $mapping->{title}->{$lang} };
+    }
+  }
 }
 
 1;
