8a2428923eaad113434fb5f259df1e845e072936
Sascha Adler <sadler@crkn.ca>
2020-01-30 19:00:27 -0700

    Replaces using sessions to keep track of search queries with a client-side solution, and removes sessions from CAP entirely #27

--- CAP/root/build/js/99.main.js
+++ CAP/root/build/js/99.main.js
@@ -1,4 +1,15 @@
 $(function() {
+  var action = location.pathname.split("/")[1];
+  if (action === "search") {
+    sessionStorage.setItem("query", $("#query").attr("value"));
+    sessionStorage.setItem("searchPath", location.href);
+  } else if (action === "view") {
+    $("#query").attr("value", sessionStorage.getItem("query"));
+    $(".matching-pages").attr("data-query", sessionStorage.getItem("query"));
+    $("#searchBackButton").removeClass("hidden");
+    $("#searchBackButton").attr("href", sessionStorage.getItem("searchPath"));
+  }
+
   $("#pvToolbar").pageViewer();
   $(".matching-pages").matchingPages();
 
