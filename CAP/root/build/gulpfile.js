261bbfca9e2a30e5665b5375bab9e0c9d003c794
Sascha Adler <sadler@crkn.ca>
2020-03-06 17:52:21 -0500

    Adds Modernizr for detecting flexbox support, shows an annoying banner over web content if the browser doesn't have it.

--- CAP/root/build/gulpfile.js
+++ CAP/root/build/gulpfile.js
@@ -47,6 +47,7 @@ const cssWatch = () => {
 const js = () => {
   return gulp
     .src([
+      "./js/early/*.js",
       "./node_modules/jquery/dist/jquery.js",
       "./node_modules/popper.js/dist/umd/popper.js",
       "./node_modules/bootstrap/dist/js/bootstrap.js",
