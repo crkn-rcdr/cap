9a9503442a44def44bf95a6136906a5ea395b8f0
Sascha Adler <sadler@crkn.ca>
2022-04-26 20:07:00 -0230

    Removes code dedicated to showing Heritage tags

--- frontend/js/03.dragonViewer.js
+++ frontend/js/03.dragonViewer.js
@@ -22,11 +22,6 @@
       var page = this.settings.initialPage;
       history.replaceState({ page: page }, null, this.makePathFromPage(page));
 
-      // seems like a good enough place to load component data (that I'm not bothering with server-side)
-      if (this.settings.hasTags) {
-        this.fetchTagData(page);
-      }
-
       this.pageUpdated(page);
       this.zoomUpdated(this.dragon.viewport.getZoom());
     },
@@ -39,13 +34,11 @@
         total: parseInt($e.attr("data-total"), 10),
         portalName: $e.attr("data-portal-name"),
         documentLabel: $e.attr("data-document-label"),
-        hasTags: !!$e.attr("data-tags"),
         imageLoadErrorString: $e.attr("data-load-error"),
       };
 
       var pv = this;
       pv.components = [];
-      var lastTaggedPage = -1;
 
       $("#pvPageSelect option").each(function (index) {
         var uriTemplate = this.getAttribute("data-uri");
@@ -58,21 +51,6 @@
           label: this.innerHTML,
           fullImage: uriTemplate.replace("$SIZE", "max").replace("$ROTATE", 0),
         };
-
-        if (pv.settings.hasTags) {
-          component.hasTags = !!parseInt(this.getAttribute("data-tags"), 10);
-
-          if (lastTaggedPage >= 0) {
-            component.previousTags = lastTaggedPage;
-          }
-
-          if (component.hasTags) {
-            for (var i = Math.max(0, lastTaggedPage); i < index; i++) {
-              pv.components[i].nextTags = index;
-            }
-            lastTaggedPage = index;
-          }
-        }
 
         pv.components.push(component);
       });
@@ -113,7 +91,6 @@
         }
         pv.isOnPopState = false;
 
-        pv.fetchTagData(page);
         pv.pageUpdated(page);
       });
 
@@ -149,12 +126,6 @@
 
       this.searchView = {
         frame: pve($("#pvSearch")),
-      };
-
-      this.tagView = {
-        frame: pve($("#pvComponent")),
-        loading: pve($("#pvComponentLoading")),
-        container: pve($("#pvComponentContainer")),
       };
     },
 
@@ -200,25 +171,6 @@
         this.tagView.frame.hide();
         this.searchView.frame.toggle();
         this.controls.searchToggle.selector.toggleClass("active");
-      };
-      this.toggleTags = function () {
-        this.searchView.frame.hide();
-        this.tagView.frame.toggle();
-        this.controls.tagToggle.selector.toggleClass("active");
-      };
-      this.previousTaggedPage = function () {
-        var page = this.dragon.currentPage();
-        var previousTags = this.components[page].previousTags;
-        if (previousTags) {
-          this.dragon.goToPage(previousTags);
-        }
-      };
-      this.nextTaggedPage = function () {
-        var page = this.dragon.currentPage();
-        var nextTags = this.components[page].nextTags;
-        if (nextTags) {
-          this.dragon.goToPage(nextTags);
-        }
       };
       this.enterFullscreen = function () {
         if (!document.fullscreenElement) {
@@ -307,21 +259,6 @@
           selection: "#pvSearchToggle",
           eventName: "click",
           handler: this.toggleSearch,
-        }),
-        tagToggle: pvc({
-          selection: "#pvTagToggle",
-          eventName: "click",
-          handler: this.toggleTags,
-        }),
-        previousTags: pvc({
-          selection: "#pvComponentPreviousLink",
-          eventName: "click",
-          handler: this.previousTaggedPage,
-        }),
-        nextTags: pvc({
-          selection: "#pvComponentNextLink",
-          eventName: "click",
-          handler: this.nextTaggedPage,
         }),
         fullscreenEnter: pvc({
           selection: "#pvFullscreenEnter",
@@ -414,26 +351,6 @@
       if ($fullImage.length > 0) {
         $fullImage.attr("href", this.components[page].fullImage);
       }
-
-      if (this.settings.hasTags) {
-        this.controls.tagToggle.enable();
-        if (this.components[page].previousTags) {
-          $("#pvComponentPreviousSeq").text(
-            this.components[this.components[page].previousTags].label
-          );
-          this.controls.previousTags.enable();
-        } else {
-          this.controls.previousTags.disable("hidden");
-        }
-        if (this.components[page].nextTags) {
-          $("#pvComponentNextSeq").text(
-            this.components[this.components[page].nextTags].label
-          );
-          this.controls.nextTags.enable();
-        } else {
-          this.controls.nextTags.disable("hidden");
-        }
-      }
     },
 
     zoomUpdated: function (zoom) {
@@ -443,43 +360,6 @@
       zoom >= this.dragon.viewport.getMaxZoom()
         ? this.controls.bigger.disable("disabled")
         : this.controls.bigger.enable();
-    },
-
-    fetchTagData: function (page) {
-      if (this.settings.hasTags) {
-        this.tagView.container.selector.html("");
-        if (this.components[page].hasTags) {
-          this.controls.tagToggle.selector.addClass("btn-info");
-          this.tagView.container.hide();
-          this.tagView.loading.show();
-          if (this.components[page].tags) {
-            this.tagView.loading.hide();
-            this.tagView.container.selector.html(this.components[page].tags);
-            this.tagView.container.show();
-          } else {
-            var call = ["", "view", this.components[page].key].join("/");
-            var pv = this;
-            $.ajax({
-              url: call,
-              dataType: "html",
-              data: { fmt: "ajax" },
-              success: function (snippet) {
-                pv.components[page].tags = snippet;
-                pv.tagView.loading.hide();
-                pv.tagView.container.selector.html(snippet);
-                pv.tagView.container.show();
-              },
-              error: function () {
-                pv.tagView.loading.hide();
-              },
-            });
-          }
-        } else {
-          this.controls.tagToggle.selector.removeClass("btn-info");
-          this.tagView.loading.hide();
-          this.tagView.container.hide();
-        }
-      }
     },
   };
 
