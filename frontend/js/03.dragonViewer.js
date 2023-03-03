915d9d8a4970166990739f2b2223949b2fde6914
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-03-03 09:54:29 -0500

    Pdf fix (#178)
    * Changes to parl - help icon and search tips page text (#132)
    
    * Minor changes for LOP (#140)
    
    * Fix identifier for "annuals"
    
    The slug in the IIIF collection editor got changed not sure when), and most of what was "annual" is now "annuals".  Fixing the CAP configuration to match what IIIF says.
    
    * Adding "University of Regina Archives" (#137)
    
    * Removing apache configuration from this repository. (#144)
    
    * Lop text updates (#147)
    
    * Fix identifier for "annuals"
    
    The slug in the IIIF collection editor got changed not sure when), and most of what was "annual" is now "annuals".  Fixing the CAP configuration to match what IIIF says.
    
    * Adding "University of Regina Archives" (#137)
    
    * Removing apache configuration from this repository. (#144)
    
    * Initial updates
    
    Co-authored-by: Russell McOrmond <rmcormond@crkn.ca>
    
    * Minor changes for LOP (#149)
    
    * Changes to parl - help icon and search tips page text (#150)
    
    * First attempt at looking for pdf in access
    
    * Fixing download
    
    * Fixes bug causing page to be broken
    
    * readable file size
    
    * file download size
    
    * File size api
    
    * Update file sizes on next
    
    * Loader on next page
    
    * English done
    
    * French labeling
    
    * Re building
    
    * fixing bug
    
    * Fixing download showing in search result
    
    * Buttons match mockup + ensure cache numbers updated
    
    * Re-add size for french
    
    * Correct french reel
    
    * French labeling for reel
    
    * english docs link
    
    * fix logo width safari
    
    * Fix logo width
    
    * Enhancements to provide more context
    
    * Original download icon
    
    * Resetting buttons
    
    ---------
    
    Co-authored-by: Russell McOrmond <rmcormond@crkn.ca>

--- frontend/js/03.dragonViewer.js
+++ frontend/js/03.dragonViewer.js
@@ -85,19 +85,15 @@
       this.dragon.addHandler("page", function (event) {
         var page = event.page;
 
-
-        var downloadButton = document.getElementById("pvFullImageDownload");
+        /*var downloadButton = document.getElementById("pvFullImageDownload");
         var slug = downloadButton.getAttribute("data-slug");
         var seq = downloadButton.getAttribute("data-seq");
 
         var url = "/files/get/" + slug + "/" + seq;
-
-
         var pvFullImageDownloadSize = document.getElementById("pvFullImageDownloadSize");
         if(pvFullImageDownloadSize) pvFullImageDownloadSize.innerHTML = '<img class="full-size-download-spinner" src="/static/images/spinner.gif"/>';
         var pvDownloadSingleSize = document.getElementById("pvDownloadSingleSize");
         if(pvDownloadSingleSize) pvDownloadSingleSize.innerHTML = '<img class="full-size-download-spinner" src="/static/images/spinner.gif"/>';
-        
         $.ajax({
           url: url,
           method: "get",
@@ -109,7 +105,7 @@
             that.$element.empty();
             that.$element.html("Error &mdash; Erreur");
           },
-        });
+        });*/
 
         if (!pv.isOnPopState) {
           history.replaceState({ page: page }, null, pv.makePathFromPage(page));
@@ -409,10 +405,10 @@
       var $singleDownload = $("#pvDownloadSingle");
       var downloadUri = this.components[page].download;
       if (downloadUri) {
-        $singleDownload.removeClass("hidden");
+        $singleDownload.prop('disabled', false);//.removeClass("hidden");
         $singleDownload.attr("href", downloadUri);
       } else {
-        $singleDownload.addClass("hidden");
+        $singleDownload.prop('disabled', true);//.addClass("hidden");
         $singleDownload.attr("href", "");
       }
 
