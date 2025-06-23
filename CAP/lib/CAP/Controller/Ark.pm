761f013606562aa12c8a0afcd8579ec4361e8d09
Brittny Lapierre <brittny.lapierre@gmail.com>
2025-06-23 07:18:23 -0400

    Adding Persistent URLs to Canadiana (#226)
    * 1.created ark.pm' 2.added ark_resolvber_base to cap.conf
    
    * Update depositors.json
    
    * added ark-url to cap.conf
    
    * Update GAC URL
    
    * testing Ark.pm by return a number
    
    * created a simple ark to test it
    
    * added log->debug in ark.pm
    
    * added log output to cap.conf and dorker-compose.yml
    
    * ark.pm works well
    
    * created an env file
    
    * remove wrong DotENv
    
    * set up a testing path to ark.pm
    
    * Update Dockerfile
    
    * added test ark url
    
    * commen off the statement on capnfile
    
    * removed invalid @ on _fetch_ark_from_solr
    
    * add / to ark_url
    
    * replaced ark_url by using legacy-n2t
    
    * get ark from ark_resolver api ,removed _fetch_ark_from_solr
    
    * fixed the issue can't display ark on about tab
    
    * Update for revisions
    
    * Adding a check for web scrapers
    
    * Update Ark.pm
    
    * Demo (#227)
    
    * Build for GAC
    
    * Saving change for removal of MPL newspapers from PUB
    
    * Update for revisions
    
    * Adding a check for web scrapers
    
    * Update Ark.pm
    
    ---------
    
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>
    
    * Only show persistent url on item page
    
    * Edit to use counter robots list
    
    * Adding counter robots check (#228)
    
    * Update GAC URL
    
    * Demo (#227)
    
    * Build for GAC
    
    * Saving change for removal of MPL newspapers from PUB
    
    * Update for revisions
    
    * Adding a check for web scrapers
    
    * Update Ark.pm
    
    ---------
    
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>
    
    * Only show persistent url on item page
    
    * Edit to use counter robots list
    
    ---------
    
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>
    
    * Removing the ark api call from search results pagination
    
    * Removing call to ark api for search results for test (#229)
    
    * Adding counter robots check (#228)
    
    * Update GAC URL
    
    * Demo (#227)
    
    * Build for GAC
    
    * Saving change for removal of MPL newspapers from PUB
    
    * Update for revisions
    
    * Adding a check for web scrapers
    
    * Update Ark.pm
    
    ---------
    
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>
    
    * Only show persistent url on item page
    
    * Edit to use counter robots list
    
    ---------
    
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>
    
    * Removing the ark api call from search results pagination
    
    ---------
    
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>
    
    * Remove counter check in favour of haproxy
    
    * Setting correct n2t url for prod
    
    ---------
    
    Co-authored-by: mary-crkn <mzheng@crkn.ca>
    Co-authored-by: mary-crkn <162184168+mary-crkn@users.noreply.github.com>

---
+++ CAP/lib/CAP/Controller/Ark.pm
@@ -0,0 +1,83 @@
+package CAP::Controller::Ark;
+use Moose;
+use namespace::autoclean;
+use JSON qw(decode_json encode_json);
+use LWP::UserAgent;
+use URI;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+#__PACKAGE__->config(namespace => 'ark:');
+
+# Receives an ARK parameter, calls the FastAPI service to retrieve the corresponding URL
+
+sub index :Path('/ark:/69429/foobar') :Args(2) {
+    my ($self, $c, $naan, $noid) = @_;
+    my $json = JSON->new->utf8->canonical->pretty;
+    my $user_agent = $c->request->user_agent;
+    my $ark = "$naan/$noid";
+    my $ark_resolver_base = $c->config->{ark_resolver_base};
+    my $ark_resolver_endpoint = "ark:/$ark";
+    my $ark_resolver_url = $ark_resolver_base . $ark_resolver_endpoint;
+  
+    # Initialize a UserAgent object
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(10);
+
+    # Get subfix from domain host if it exists
+    my $current_host = $c->request->uri->host;
+    my @split_parts = split /\./,$current_host;
+    my $first_part = $split_parts[0];
+    my $subfix;
+    if ($first_part =~ /-([^-]+)/) {
+        $subfix = $1
+    }
+
+    # Build a query param
+    my %query_params = ();
+    if (defined $subfix ){
+        $query_params{q} = $subfix;
+    }
+
+    # Build an url with a query params
+    my $ark_url_query = URI->new($ark_resolver_url);
+    $ark_url_query->query_form(%query_params) if %query_params; 
+    
+    # Call ark-resolver to get a redirect url
+    my $response = $ua->get($ark_url_query);
+    if ($response->is_success) {
+        my $data; 
+        my $content = $response->decoded_content;
+        eval {
+            $data = $json->decode($content);
+        };
+        if ($@) {
+            $c->detach('/error', [500, "JSON parsing error"]);
+            return;
+        }
+        my $url = $data->{url};
+          
+         
+        if ($url) {
+            $c->response->redirect($url);
+            $c->detach();       
+        } 
+        else {
+            $c->detach('/error', [500, "URL not found"]);
+            return;
+        }
+         
+    }
+    else {
+        if ($response->code == 404) {
+            $c->detach('/error', [404, "ARK not found"]);
+        }
+        else {
+            $c->detach('/error', [500, "FastAPI service error"]);
+        }
+    }    
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1; 
